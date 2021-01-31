7c874e55f7d87ac9414e1e55045df8bf3c1e77db
ferhatcan <ataman.ferhatcan@gmail.com>
2021-01-31 20:36:43 +0300

    config parser initialized

---
+++ utils/configParser.py
@@ -0,0 +1,41 @@
+from configparser import ConfigParser
+import importlib
+import datetime
+
+class options:
+    def __init__(self, config_file_name):
+        self.config = ConfigParser()
+        self.config.read(config_file_name)
+
+        self.argsCommon = ParseCommons(self.config)
+        self.argsDataset = ParseDataset(self.config)
+
+
+class ParseCommons:
+    def __init__(self, config: ConfigParser):
+        self.experiment_name    = config["DEFAULT"]["experiment_name"]
+        self.generateNew        = config["DEFAULT"].getboolean("generate_new_experiment")
+        self.device             = config["HARDWARE"]["device"]
+        self.seed               = int(config["HARDWARE"]["seed"])
+        self.n_GPUs             = int(config["HARDWARE"]["n_GPUs"])
+        self.precision          = config["HARDWARE"]["precision"]
+        self.model              = config["MODEL"]["model"]
+        self.input_shape           = list(map(int, config["DATASET"]["input_shape"].split(',')))
+        self.batch_size         = int(config["DATASET"]["batch_size"])
+        if self.generateNew:
+            self.experiment_save_path = "runs/" + self.model + "/" \
+            + self.experiment_name + "_" + datetime.datetime.now().strftime('%Y-%m-%d-hour%H')
+        else:
+            self.experiment_save_path = "runs/" + self.model + "/" \
+                                        + self.experiment_name
+
+
+class ParseDataset(ParseCommons):
+    def __init__(self, config: ConfigParser):
+        super(ParseDataset, self).__init__(config)
+
+        self.train_set_paths = config["DATASET"]["train_set_paths"].split(',\n')
+        self.test_set_paths = config["DATASET"]["test_set_paths"].split(',\n')
+
+        self.normalize = config["DATASET"]["normalize"]
+        self.validation_size = float(config["DATASET"]["validation_size"])
