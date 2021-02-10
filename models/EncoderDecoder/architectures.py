1e60e9360a7c8f2ca9d236249cf55441232bf8bb
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-10 22:54:06 +0300

    encoder decoder model is constructed.

--- models/EncoderDecoder/architectures.py
+++ models/EncoderDecoder/architectures.py
@@ -3,19 +3,18 @@ import torch.nn as nn
 from models.IModel import IModel
 from models.EncoderDecoder.custom_layers import Encoder, Decoder
 
+
 class EncoderDecoder_v01(IModel):
     def __init__(self, args):
-        super(EncoderDecoder_v01, self).__init__(args)
+        super(EncoderDecoder_v01, self).__init__()
 
-        self.encoder1 = Encoder(args)
-        self.encoder2 = Encoder(args)
+        self.encoder = Encoder(args)
         self.decoder = Decoder(args)
 
-    def forward(self, x):
-        out1 = self.encoder1(x['inputs'])
-        out2 = self.encoder2(x)
-
-
-        out = self.decoder({'layers': [out1, out2]})
-
+    def forward(self, inputs: list):
+        assert len(inputs) == 1, 'There should be 1 input'
+        feats = self.encoder(inputs[0])
+        out = self.decoder(feats)
         return out
+
+# @TODO EMRE TEST
--- models/EncoderDecoder/custom_layers.py
+++ models/EncoderDecoder/custom_layers.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from models.IModel import IModel
@@ -7,21 +8,69 @@ class Encoder(IModel):
     def __init__(self, args):
         super(Encoder, self).__init__(args)
 
-        self.encoder = nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.input_dim = args.input_dim if 'input_dim' in args else 3
+        self.maxpool = nn.AvgPool2d(2)
+
+        self.encoder_layer_0 = nn.Conv2d(self.input_dim, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.encoder_layer_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.encoder_layer_2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.encoder_layer_3 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False)
+        self.encoder_layer_4 = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1, bias=False)
 
     def forward(self, x):
-        out = self.encoder(x)
+        output = dict()
+        output['input_layer'] = self.maxpool(self.encoder_layer_0(x))
+        output['layer1'] = self.maxpool(self.encoder_layer_1(output['input_layer']))
+        output['layer2'] = self.maxpool(self.encoder_layer_2(output['layer1']))
+        output['layer3'] = self.maxpool(self.encoder_layer_3(output['layer2']))
+        output['layer4'] = self.maxpool(self.encoder_layer_4(output['layer3']))
 
-        return out
+        return output
 
 
 class Decoder(IModel):
     def __init__(self, args):
         super(Decoder, self).__init__(args)
 
-        self.decoder = nn.ConvTranspose2d(128, 3, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.args = args
 
-    def forward(self, x):
-        out = self.decoder(x)
+        self.output_dim = args.output_dim if 'output_dim' in args else 3
+
+        assert (args.scale % 2 == 0 or args.scale == 1), 'Scale should be multiple of 2 or be 1'
+
+        self.layer1 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.layer2 = nn.ConvTranspose2d(1024, 256, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.layer3 = nn.ConvTranspose2d(512, 128, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.layer4 = nn.ConvTranspose2d(256, 64, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.layer5 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=(1, 1), groups=32, bias=False)
+        self.layer6 = nn.Conv2d(64, self.output_dim, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.tanh = nn.Tanh()
+
+        self.output_layer = nn.Sequential(
+            self.layer6,
+            self.tanh,
+        )
+
+    def forward(self, feats):
+        assert len(feats) == 5, 'There should be 5 layer outputs for decoder!'
+
+        out = self.layer1(feats['layer4'])
+        out = torch.cat((feats['layer3'], out), dim=1)
+
+        out = self.layer2(out)
+        out = torch.cat((feats['layer2'], out), dim=1)
+
+        out = self.layer3(out)
+        out = torch.cat((feats['layer1'], out), dim=1)
+
+        out = self.layer4(out)
+        out = torch.cat((feats['input_layer'], out), dim=1)
+
+        out = self.layer5(out)
+
+        out = self.output_layer(out)
 
         return out
+
+# @TODO EMRE TEST
