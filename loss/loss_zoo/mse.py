cc309ebfa91599a0fd79d4d8004e8416b8f63020
Emre <yekilicer@innovita.com.tr>
2021-02-11 13:11:28 +0300

    function modified according to input type

--- loss/loss_zoo/mse.py
+++ loss/loss_zoo/mse.py
@@ -11,12 +11,9 @@ class MSELossLocal(ILoss):
 
     def forward(self, x: dict) -> list:
         assert "gts" in x and "result" in x, "gts Type should be a dict and contains \"gts\" and \"result\" keys"
-        assert len(x["result"]) == 1, "there should be 1 result to calculate loss"
         result = []
-        # only calculate with visible image
-        for i in range(0, len(x["gts"])):
-            if x["gts"][i].shape == x["result"][0].shape:
-                result.append(self.loss_function(x["gts"][i], x["result"][0]))
+        if x["gts"].shape == x["result"].shape:
+            result.append(self.loss_function(x["gts"], x["result"]))
         return result
 
 # @TODO EMRE
