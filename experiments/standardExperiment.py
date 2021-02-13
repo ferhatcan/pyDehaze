2160b73a2cf93ab031b7180b3725833a12aa3099
Emre <yekilicer@innovita.com.tr>
2021-02-13 12:38:42 +0300

    train_one_epoch, save and load methods implemented

--- experiments/standardExperiment.py
+++ experiments/standardExperiment.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 
 from experiments.IExperiment import IExperiment
@@ -24,16 +26,38 @@ class StandardExperiment(IExperiment):
         self.benchmark = benchmark
         self.logger = logger
 
+        self.num_of_loss = len(self.loss["loss_types"])
+
         self.max_epoch = args.epoch_num
         self.device = torch.device("cuda:0" if torch.cuda.is_available() and args.device == "gpu" else "cpu")
 
+        self.records = dict()
+        self.records["model_state_dict"] = self.model.state_dict()
+        self.records["optimizer"] = self.optimizer.state_dict()
+        self.records["lr_scheduler"] = self.lr_scheduler.state_dict()
+        self.records["current_epoch"] = 0
+
+        self.destination_checkpoint = ".pretrainednetworks"
+
     def train_one_epoch(self):
-        raise NotImplementedError
+        total_losses = [0] * self.num_of_loss
+        for data in self.dataloaders["train"]:
+            data["inputs"] = data["inputs"].to(self.device)
+            data["gts"] = data["gts"].to(self.device)
+            _, losses = self.method.train(data)
+            for index, loss in enumerate(losses):
+                total_losses[index] = total_losses[index] + loss.cpu().detach().numpy()
+        self.lr_scheduler.step()
+        average_losses = [x / (len(self.dataloaders["train"])) for x in total_losses]
+        return average_losses
 
     def train(self):
-        for epoch in range(self.max_epoch):
+        self.load("model_last")
+        for epoch in range(self.records["current_epoch"], self.max_epoch):
             avg_train_loss = self.train_one_epoch()
             avg_valid_loss, benchmark_results = self.test_dataset(dataloader=self.dataloaders['validation'])
+            self.records["current_epoch"] += 1
+            self.save("model_last")
 
     def test_dataset(self, dataloader):
         raise NotImplementedError
@@ -45,7 +69,20 @@ class StandardExperiment(IExperiment):
         raise NotImplementedError
 
     def save(self, saveName: str):
-        raise NotImplementedError
+        self.records["model_state_dict"] = self.model.state_dict()
+        self.records["optimizer"] = self.optimizer.state_dict()
+        self.records["lr_scheduler"] = self.lr_scheduler.state_dict()
+        savePath = os.path.join(self.destination_checkpoint, saveName) + '.pth'
+        torch.save(self.records, savePath)
 
     def load(self, loadName: str):
-        raise NotImplementedError
+        loadPath = os.path.join(self.destination_checkpoint, loadName) + '.pth'
+        if os.path.exists(loadPath):
+            state_dicts = torch.load(loadPath)
+            self.records["model_state_dict"] = state_dicts["model_state_dict"]
+            self.records["optimizer"] = state_dicts["optimizer"]
+            self.records["lr_scheduler"] = state_dicts["lr_scheduler"]
+            self.records["current_epoch"] = state_dicts["current_epoch"]
+            self.model.load_state_dict(self.records["model_state_dict"])
+            self.optimizer.load_state_dict(self.records["optimizer"])
+            self.lr_scheduler.load_state_dict(self.records["lr_scheduler"])
