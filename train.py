31c06b2d1d9042d1d356922dfa4fbd2fa952d945
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-14 16:22:38 +0300

    train file is created

---
+++ train.py
@@ -0,0 +1,10 @@
+from assemblers.assemblerGetter import getExperimentWithDesiredAssembler
+
+CONFIG_FILE_NAME = './configs/EncoderDecoder_v01.ini'
+
+def main():
+    experiment = getExperimentWithDesiredAssembler(CONFIG_FILE_NAME)
+    experiment.train()
+
+if __name__ == '__main__':
+    main()
