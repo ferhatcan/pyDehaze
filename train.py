b3640eefacfc4d19b827910768ddf23877431ff8
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-16 11:01:58 +0300

    training with new config

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from assemblers.assemblerGetter import getExperimentWithDesiredAssembler
 
-CONFIG_FILE_NAME = './configs/EncoderDecoder_v01.ini'
+CONFIG_FILE_NAME = './configs/EncoderDecoder_v02.ini'
 
 def main():
     experiment = getExperimentWithDesiredAssembler(CONFIG_FILE_NAME)
