736f9faeb73dd42df441a4ef9f53cdedbf068147
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-16 11:00:05 +0300

    v02 version is added

--- models/model_zoo/EncoderDecoder/__init__.py
+++ models/model_zoo/EncoderDecoder/__init__.py
@@ -5,5 +5,7 @@ __all__ = ['EncoderDecoder_v01']
 def make_model(args):
     if args.type == 'v01':
         return EncoderDecoder_v01(args)
+    elif args.type == 'v02':
+        return EncoderDecoder_v02(args)
     else:
         raise NotImplementedError('There is no such an architecture in Encoder-Decoder Network: {:}'.format(args.type))
