52a974c2e158ec94260b5e813a539e835a09fd34
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-28 12:47:26 +0300

    new model supports added

--- assemblers/assemblerGetter.py
+++ assemblers/assemblerGetter.py
@@ -5,7 +5,9 @@ from assemblers.encoderDecoder_v01Assembler import getExperiment as encoderDecod
 def getExperimentWithDesiredAssembler(config_file_name):
     args = options(config_file_name)
     if args.argsCommon.model == 'encoderDecoder':
-        if args.argsModel.type == 'v01' or 'v02':
+        if args.argsModel.type == 'v01' or args.argsModel.type == 'v02' \
+                or args.argsModel.type == 'resnetv01' or args.argsModel.type == 'resnetv02'\
+                or args.argsModel.type == 'resnetv01-DIV2K':
             return encoderDecoder_v01(args)
         else:
             ValueError('There should be an valid assembler type: given type {:}'.format(args.argsModel.type))
