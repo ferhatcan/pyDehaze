4f5ef1c71890cffed9a5179679c8da7dd940987f
ferhatcan <ataman.ferhatcan@gmail.com>
2021-02-28 12:54:59 +0300

    resnet architecture support is added

--- models/model_zoo/EncoderDecoder/__init__.py
+++ models/model_zoo/EncoderDecoder/__init__.py
@@ -7,5 +7,9 @@ def make_model(args):
         return EncoderDecoder_v01(args)
     elif args.type == 'v02':
         return EncoderDecoder_v02(args)
+    elif args.type == 'resnetv01':
+        return EncoderDecoderResnet(args)
+    elif args.type == 'resnetv02':
+        return EncoderDecoderResnetv02(args)
     else:
         raise NotImplementedError('There is no such an architecture in Encoder-Decoder Network: {:}'.format(args.type))
